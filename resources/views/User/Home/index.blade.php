0e711842cce5c4870f3cd8e652d709595f7002e7
zabilsabri <zabilsabrimuhammad0@gmail.com>
2023-05-23 08:39:07 +0800

    user home OP

--- resources/views/User/Home/index.blade.php
+++ resources/views/User/Home/index.blade.php
@@ -1,4 +1,12 @@
 @extends('User.layout.app', ['title' => 'Home'])
 
 @section('content')
+<h3 class="title-blue" >Selamat Datang</h3>
+<p class="text-blue text-center" >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mollis, augue malesuada porttitor sodales, augue ante venenatis mi, at tincidunt nunc magna quis nulla. Curabitur vestibulum nibh orci. Curabitur aliquet diam in dolor scelerisque, sed sollicitudin nulla finibus. Praesent a placerat erat, vel varius mauris. Praesent tincidunt augue ultricies faucibus sollicitudin. Morbi ut sapien nec mauris tempus cursus suscipit vitae ante. Etiam vulputate sapien sit amet tellus vulputate sollicitudin. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque quis lacinia sem.</p>
+    <div class="grid gap-3">
+        <div class="p-2 g-col-6">Grid item 1</div>
+        <div class="p-2 g-col-6">Grid item 2</div>
+        <div class="p-2 g-col-6">Grid item 3</div>
+        <div class="p-2 g-col-6">Grid item 4</div>
+    </div>
 @endsection
