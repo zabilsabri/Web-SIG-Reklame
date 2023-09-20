5ff7171c226f5a81753998157f7119e50407b2e0
zabilsabri <zabilsabrimuhammad0@gmail.com>
2023-09-21 00:58:24 +0800

    `fix`: Pimpinan filter fixed

--- resources/views/Pimpinan/Layout/app.blade.php
+++ resources/views/Pimpinan/Layout/app.blade.php
@@ -134,7 +134,7 @@
 
         });
 
-        $('div.btn-sr').html('<select id="categoryFilterStatus" style="width: auto;" class="form-control special"><option value="">Status (Show All)</option><option value="Belum Disewa">Belum Disewa</option><option value="Sudah Disewa">Sudah Disewa</option><option value="Mendekati Jatuh Tempo">Mendekati Jatuh Tempo</option><option value="Melebihi Jatuh Tempo">Melebihi Jatuh Tempo</option></select>');
+        $('div.btn-sr').html('<select id="categoryFilterStatus" style="width: auto;" class="form-control special"><option value="">Status (Show All)</option><option value="Belum Disewa">Belum Disewa</option><option value="Sedang Disewa">Sedang Disewa</option><option value="Selesai">Selesai</option><option value="Mendekati Jatuh Tempo">Mendekati Jatuh Tempo</option><option value="Melebihi Jatuh Tempo">Melebihi Jatuh Tempo</option></select>');
 
         var table = $('#tableMonitorReklame').DataTable();
 
