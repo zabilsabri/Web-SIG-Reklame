987c68c1f7dcf64f70fb8465a0a235f31a307ac7
zabilsabri <zabilsabrimuhammad0@gmail.com>
2023-09-22 21:00:59 +0800

    `feat`: status based on year

--- resources/views/Admin/layout/Table Button/status2.blade.php
+++ resources/views/Admin/layout/Table Button/status2.blade.php
@@ -5,6 +5,22 @@
 <p class="status-monitor-yellow p-2 m-0" ><img class="me-1" src="{{ asset('img/clock yellow.png') }}" alt="">Mendekati Jatuh Tempo</p>
 
 @else
-<p class="status-monitor-green p-2 m-0" ><img class="me-1" src="{{ asset('img/green check.png') }}" width="25px" alt="">Selesai</p>
+
+@php
+$date = strtotime($data->tgl_jatuh_tempo);
+$reklameYear = date('Y', $date);
+
+$currentDate = new DateTime();
+$thisYear = $currentDate->format("Y");
+
+@endphp
+
+@if ($reklameYear == $thisYear)
+    <p class="status-monitor-red p-2 m-0"><img class="me-1" src="{{ asset('img/danger red.png') }}" width="25px" alt="">Melebihi Jatuh Tempo</p>
+@else
+    <p class="status-monitor-green p-2 m-0">
+        <img class="me-1" src="{{ asset('img/green check.png') }}" width="25px" alt="">Selesai
+    </p>
+@endif
 
 @endif
